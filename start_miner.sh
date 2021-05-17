e39bd97da35c0794f7e280a9f1a001a8835f8cf2
SwitchAlpha <48888576+SwitchAlpha@users.noreply.github.com>
2021-05-17 11:15:27 +0300

    Added

---
+++ start_miner.sh
@@ -0,0 +1,6 @@
+#
+# Example shell file for starting PhoenixMiner.exe to mine ETH
+#
+
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./PhoenixMiner -pool ethash-eu.unmineable.com:3333 -wal DOGE:DHxKgzLZ9g9Er85UXbW39stpRpjG55LHRp.GGL
